0975976e5626f137a096a222c1cce7b3e300f897
SanMarAbr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-13 14:06:46 -0600

    Modificacion al programa se agregan sobrecarga constructores

--- src/Operaciones/PruebaAritmetica.java
+++ src/Operaciones/PruebaAritmetica.java
@@ -4,14 +4,14 @@ public class PruebaAritmetica {
     public static void main(String[] args) {
         
         Aritmetica aritmetica1 = new Aritmetica();
-        aritmetica1.a = 3;
-        aritmetica1.b = 2;
-        aritmetica1.sumar();
+        System.out.println("Aritmetica1 a: "+aritmetica1.a);
+        System.out.println("Aritmetica1 b: "+aritmetica1.b);
+        
+        Aritmetica aritmetica2 = new Aritmetica(5,8);
+        System.out.println("Aritmetica2 a : "+aritmetica2.a);
+        System.out.println("Aritmetica2 b : "+aritmetica2.b);
+        
 
-        int resultado = aritmetica1.sumarConRetorno();
-        System.out.println("Resultado desde la prueba = "+resultado);
 
-        resultado = aritmetica1.sumarConArgumentos(5, 8);
-        System.out.println("Resultado usando argumentos = "+resultado);
     }   
 }
