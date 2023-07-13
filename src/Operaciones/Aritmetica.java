735d78f62f98fa7e69230174d0bbcd1f8d9c1e66
SanMarAbr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-13 13:40:42 -0600

    Se agregan conceptos de constructores vacios

--- src/Operaciones/Aritmetica.java
+++ src/Operaciones/Aritmetica.java
@@ -1,29 +1,42 @@
 package Operaciones;
 
 public class Aritmetica {
-    //Atributos de la clase
+    // Atributos de la clase
     int a;
     int b;
-    //Metodo
-    public void sumar(){
-        int resultado = a + b;
-        System.out.println("resultado = "+resultado);
+
+    // Constructor vacio
+    public Aritmetica() {
+        System.out.println("Ejecutando constructor");
+        
     }
 
-    public int sumarConRetorno(){
-       // int resultado = a + b;
-       // return resultado;  
+    public Aritmetica(int a, int b){
+        this.a = a;
+        this.b = b;
+        System.out.println("Ejecutando constructor con arg");
+    }
+
+    // Metodo
+    public void sumar() {
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+
+    public int sumarConRetorno() {
+        // int resultado = a + b;
+        // return resultado;
         return this.a + this.b;
     }
 
-    public int sumarConArgumentos(int a, int b){
+    public int sumarConArgumentos(int a, int b) {
         this.a = a; // El argumento a se asigna al atributo this.a
         this.b = b;
-        //return a + b;     
-        return this.sumarConRetorno(); 
-        //This solo se utiliza en la clase donde se trabaja
-        //this son los atributos de la clase
-        //Metodo de la clase 
-        
+        // return a + b;
+        return this.sumarConRetorno();
+        // This solo se utiliza en la clase donde se trabaja
+        // this son los atributos de la clase
+        // Metodo de la clase
+
     }
 }
