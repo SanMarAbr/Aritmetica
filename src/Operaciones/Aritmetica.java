e7a8695a3958b11efb06ad3f2a41bd9c428327ed
SanMarAbr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-13 13:23:32 -0600

    this

--- src/Operaciones/Aritmetica.java
+++ src/Operaciones/Aritmetica.java
@@ -12,14 +12,18 @@ public class Aritmetica {
 
     public int sumarConRetorno(){
        // int resultado = a + b;
-       // return resultado; 
-        return a + b;
+       // return resultado;  
+        return this.a + this.b;
     }
 
-    public int sumarConArgumentos(int arg1, int arg2){
-        a = arg1;
-        b = arg2;
+    public int sumarConArgumentos(int a, int b){
+        this.a = a; // El argumento a se asigna al atributo this.a
+        this.b = b;
         //return a + b;     
-        return sumarConRetorno();
+        return this.sumarConRetorno(); 
+        //This solo se utiliza en la clase donde se trabaja
+        //this son los atributos de la clase
+        //Metodo de la clase 
+        
     }
 }
